816bb4590049db4579792e026c1ec17244688705
sefortescue <yzj8b4ztkh9cmhxswqtz@tutanota.com>
2022-07-30 10:25:34 -0600

    add book status option.

---
+++ scripts/stat_book.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import sys
+
+def stat_book():
+    conn = sqlite3.connect('local.db')
+    curs = conn.cursor()
+    if sys.argv[2] == "0":
+        curs.execute("UPDATE books SET status=0 WHERE id=?", (sys.argv[1]))
+    elif sys.argv[2] == "1":
+        curs.execute("UPDATE books SET status=1 WHERE id=?", (sys.argv[1]))
+    elif sys.argv[2] == "2":
+        curs.execute("UPDATE books SET status=2 WHERE id=?", (sys.argv[1])) 
+    else:
+        exit(1) 
+    conn.commit()
+    conn.close()
+if __name__ == "__main__":
+    stat_book()
