f88aa76b865ba8346135fc2476b903d5cfb7d22f
sefortescue <yzj8b4ztkh9cmhxswqtz@tutanota.com>
2022-08-02 09:06:51 -0600

    add book list

--- scripts/prog_book.py
+++ scripts/prog_book.py
@@ -9,8 +9,8 @@ def prog_book():
     curs = conn.cursor()
     today = date.today()
     curs.execute("INSERT INTO apus(id, date) VALUES(?, ?)", (sys.argv[1], today.strftime("%B %d, %Y")
-))
-    curs.execute("UPDATE books SET count = count + 1 WHERE id=?", (sys.argv[1]))
+,))
+    curs.execute("UPDATE books SET count = count + 1 WHERE id=?", (sys.argv[1],))
     conn.commit()
     conn.close()
 if __name__ == "__main__":
